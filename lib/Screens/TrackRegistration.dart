236ffe53773c6792d7b153e47642192243b25561
Indira <dharshinibalamurugan31@gmail.com>
2020-08-15 21:41:15 +0530

    added new participantM lists

--- lib/Screens/TrackRegistration.dart
+++ lib/Screens/TrackRegistration.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:party_man/models/FireBaseDB.dart';
 import 'package:party_man/models/ParticipationM.dart';
 import 'package:provider/provider.dart';
@@ -14,11 +15,19 @@ class TrackRegistration extends StatefulWidget {
 
 class _TrackRegistrationState extends State<TrackRegistration> {
   String id;
-
+  static const platform = const MethodChannel('sendSms');
+  List<ParticipantM> partiR=[];
+  List<ParticipantM> partiC=[];
+  List<ParticipantM> partiNA=[];
+  
+  bool checkboxvalue=false;
   _TrackRegistrationState(this.id);
-
+ Widget res;
   FireBaseDB fbObj = FireBaseDB();
   int status=0;
+
+
+
   @override
   Widget build(BuildContext context) {
     // int number= 9090909030072032;
@@ -33,9 +42,12 @@ class _TrackRegistrationState extends State<TrackRegistration> {
                 .where('partyId', isEqualTo: id)
                 .snapshots(),
             builder: (context, snapshot) {
+                partiR=[];
+                partiC=[];
+                partiNA=[];
                 if(!snapshot.hasData){ return Center(
                   child: SpinKitRotatingCircle(
-                    color: Colors.lightBlue,
+                    color: Theme.of(context).primaryColorLight,
                     size: 50.0,
                   ),
                 );}
@@ -45,14 +57,31 @@ class _TrackRegistrationState extends State<TrackRegistration> {
                   if (snapshot.data.documents[position]['gueststatus'] == 0)
                     {registered =registered+
                     snapshot.data.documents[position]['membercount'];
+
+                    partiR.add(ParticipantM(snapshot.data.documents[position]['partyid'],snapshot.data.documents[position]['hostgkey'],snapshot.data.documents[position]['guestgkey'],
+                                          snapshot.data.documents[position]['membercount'],snapshot.data.documents[position]['timeslab'],snapshot.data.documents[position]['gueststatus'],
+                                          snapshot.data.documents[position]['guestname'],snapshot.data.documents[position]['guestmobile'],snapshot.data.documents[position]['guestemail'],
+                                          snapshot.data.documents[position]['guestwhats']));
                   }
                   if (snapshot.data.documents[position]['gueststatus'] == 1) {
                     confirmed = confirmed +
                         snapshot.data.documents[position]['membercount'];
+
+                    partiC.add(ParticipantM(snapshot.data.documents[position]['partyid'],snapshot.data.documents[position]['hostgkey'],snapshot.data.documents[position]['guestgkey'],
+                        snapshot.data.documents[position]['membercount'],snapshot.data.documents[position]['timeslab'],snapshot.data.documents[position]['gueststatus'],
+                        snapshot.data.documents[position]['guestname'],snapshot.data.documents[position]['guestmobile'],snapshot.data.documents[position]['guestemail'],
+                        snapshot.data.documents[position]['guestwhats']));
+
                   }
                    if (snapshot.data.documents[position]['gueststatus'] == 2)
                     notattending =notattending+
                     snapshot.data.documents[position]['membercount'];
+
+                  partiNA.add(ParticipantM(snapshot.data.documents[position]['partyid'],snapshot.data.documents[position]['hostgkey'],snapshot.data.documents[position]['guestgkey'],
+                      snapshot.data.documents[position]['membercount'],snapshot.data.documents[position]['timeslab'],snapshot.data.documents[position]['gueststatus'],
+                      snapshot.data.documents[position]['guestname'],snapshot.data.documents[position]['guestmobile'],snapshot.data.documents[position]['guestemail'],
+                      snapshot.data.documents[position]['guestwhats']));
+
                 }
 
                 //debugPrint(snapshot.data.documents.length.toString()); //working
@@ -67,9 +96,9 @@ class _TrackRegistrationState extends State<TrackRegistration> {
                         GestureDetector(
                           onTap: (){
                             debugPrint('tapped');
-                            setState(() {
-                              status=0;
-                            });
+                            //debugPrint(partiR[0].memberCount.toString());
+                           // status=0;
+                            //printNames(snapshot, context, 0);
                           },
                           child: CircleAvatar(child: Text('$registered \n rcount'),
                             backgroundColor: Theme
@@ -81,9 +110,8 @@ class _TrackRegistrationState extends State<TrackRegistration> {
                         GestureDetector(
                           onTap: (){
                             debugPrint('tapped');
-                            setState(() {
-                              status=1;
-                            });
+                            //status=1;
+                            //printNames(snapshot, context, 1);
                           },
                           child: CircleAvatar(child: Text('$confirmed \n ccount'),
                             backgroundColor: Theme
@@ -95,9 +123,8 @@ class _TrackRegistrationState extends State<TrackRegistration> {
                         GestureDetector(
                           onTap: (){
                             debugPrint('tapped');
-                            setState(() {
-                              status=2;
-                            });
+                            //status=2;
+                              //printNames(snapshot, context, 2);
                           },
                           child: CircleAvatar(child: Text('$notattending \n nacount'),
                             backgroundColor: Theme
@@ -108,7 +135,8 @@ class _TrackRegistrationState extends State<TrackRegistration> {
                       ],
                     ),
                     sizedBox(),
-                    printNames(snapshot, context, status)
+                   PrintList(partiR),
+                   //printNames(snapshot, context, status),
                     //Text(snapshot.data.documents[postion]['gueststatus'].toString()),
                   ],
                 );
@@ -120,22 +148,94 @@ class _TrackRegistrationState extends State<TrackRegistration> {
   Widget sizedBox(){
     return SizedBox(height: 5.0);
   }
+
+  Widget PrintList(List<ParticipantM> partiR){
+    int i;
+    int len=partiR.length;
+    return Expanded(
+      child: ListView.builder(
+        itemCount: len,
+        itemBuilder: (context,int pos){
+          return Card(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+              Expanded(
+              flex:2,
+              child: Text(
+                partiR[pos].guestName,style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+              Expanded(
+                flex:2,
+                child: Text(
+                    partiR[pos].guestMobile,style: TextStyle(fontSize: 15.0,fontStyle: FontStyle.italic),
+                ),
+              ),
+              Expanded(
+                flex:1,
+                child: Checkbox(
+                  value: checkboxvalue,
+                  onChanged: (bool value) {
+                    setState(() {
+                      checkboxvalue = value;
+                    });
+                  },
+                ),
+              )
+          ])
+          );
+        },
+      ),
+    );
+
+  }
+  //partiR[pos].memberCount.toString()
   Widget printNames(snapshot,BuildContext context,int status){
+    debugPrint('inside printnames' + status.toString());
     return Expanded(
       child: ListView.builder(
         itemCount: snapshot.data.documents.length,
         itemBuilder:(context,int position){
-           //debugPrint(snapshot.data.documents[position]['gueststatus'].toString());
-           //debugPrint(snapshot.data.documents[position]['membercount'].toString());
-           //debugPrint(snapshot.data.documents.length.toString());
-          if(snapshot.data.documents[position]['gueststatus']==status) {
+           debugPrint(snapshot.data.documents[position]['gueststatus'].toString());
+           debugPrint(snapshot.data.documents[position]['membercount'].toString());
+           debugPrint(snapshot.data.documents.length.toString());
+          if(snapshot.data.documents[position]['gueststatus']==status) {////snapshot.data.documents[position]['membercount'].toString()),
             return Card(
-              elevation: 5,
-              child: Text(
-                  snapshot.data.documents[position]['membercount'].toString()),
+              //color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0)
+              ),
+              //elevation: 5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Expanded(
+                    flex:2,
+                    child: Text(
+                                 "Indira Priyadharshini",style: TextStyle(fontSize: 20.0),
+                 ),
+                  ),
+                  Expanded(
+                    flex:2,
+                    child: Text(
+                      "dharshinibalamurugan31@gmail.com \n 9898989898",style: TextStyle(fontSize: 15.0,fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  Expanded(
+                    flex:1,
+                    child: Checkbox(
+                      value: checkboxvalue,
+                      onChanged: (bool value) {
+                        setState(() {
+                          checkboxvalue = value;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
             );
-          }
-          else{
           }
         },
       ),
